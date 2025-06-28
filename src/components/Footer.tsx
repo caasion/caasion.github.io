c595e774133f7fd56c43e358c5fb7a70d6e59a3d
caasion <103465188+caasion@users.noreply.github.com>
2025-06-28 11:04:47 -0400

    Refactor Footer + Update Main Page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FaInstagram, FaGithub, FaLinkedin  } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-700'>
+      <div className='flex justify-between'>
+        <div className='footer-container'>
+          <h2>Isaac Ng</h2>
+          <p>Graduated High School Student</p>
+        </div>
+        <div className='footer-container'>
+          <h2>Contacts</h2>
+          <p><b>Email:</b> isaacngcaasi@gmail.com</p>
+          <p><b>Discord:</b> caasi.bun</p>
+        </div>
+        <div className='footer-container'>
+          <h2>Socials</h2>
+          <div className='flex items-center'>
+            <a href="https://www.instagram.com/caasi.bun/">
+            <button className='socials-button' type="button"><FaInstagram /></button>
+            </a>
+            <a href="https://github.com/caasion">
+              <button className='socials-button' type="button"><FaGithub /></button>
+            </a>
+            <a href="https://www.linkedin.com/in/isaac-ng-239412343/">
+              <button className='socials-button' type="button"><FaLinkedin /></button>
+            </a>
+            <a href="mailto:isaacngcaasi@gmail.com">
+              <button className='socials-button' type="button"><MdEmail /></button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
