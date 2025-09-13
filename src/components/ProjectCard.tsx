509573c905ab1e92d0048822108b4eab26811e1c
caasion <103465188+caasion@users.noreply.github.com>
2025-09-13 16:29:08 -0400

    Add Project Images

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,23 +3,30 @@ export interface ProjectProps {
     description: string;
     tags: string[];
     link: string;
+    img: string;
 }
 
-export default function ProjectCard({ title, description, tags, link }: ProjectProps) {
+export default function ProjectCard({ title, description, tags, link, img }: ProjectProps) {
   return (
-    <div className="rounded-2xl shadow-md hover:shadow-lg transition p-4">
-      <div>
-        <h2 className="text-xl font-bold">{title}</h2>
-        <p className="text-gray-600 mt-2">{description}</p>
-        <div className="flex gap-2 mt-3 flex-wrap">
-          {tags.map((tag: string) => (
-            <span key={tag} className="text-sm bg-gray-200 px-2 py-1 rounded-md">{tag}</span>
-          ))}
+    <div className="rounded-2xl bg-gray-700 shadow-md hover:shadow-lg transition p-4 mt-5">
+      <div className="flex">
+        <div className="flex-2/7 p-5 max-w-fit max-h-fit">
+          <img src={img} className="object-cover object-left-top w-full h-full"></img>
         </div>
-        <div className="mt-4">
-          <a href={link} target="_blank">View</a>
+        <div className="flex-5/7">
+          <h2 className="text-xl font-bold">{title}</h2>
+          <div className="flex gap-2 mt-3 flex-wrap">
+            {tags.map((tag: string) => (
+              <span key={tag} className="text-sm bg-purple-900 px-2 py-1 rounded-md">{tag}</span>
+            ))}
+          </div>
+          <p className="text-gray-200">{description}</p>
+          <div className="mt-2 rounded-2xl bg-purple-800 text-2xl px-4 py-1 w-fit">
+            <a href={link} target="_blank">View</a>
+          </div>
         </div>
       </div>
+      
     </div>
   )
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,10 @@
 import type { ProjectProps } from "../components/ProjectCard"
 import ProjectCard from "../components/ProjectCard";
+import llx from "../assets/learners-lodge-logo.png";
+import aiTodoList from "../assets/ai-todo-list.png";
+import reactMovieApp from "../assets/react-movie-app.png";
+import typingTransformer from "../assets/typing-transformer.png";
+import ultimatePlanner from "../assets/obsidian-ultimate-planner.png";
 
 const projectList: ProjectProps[] = [
   {
@@ -7,35 +12,40 @@ const projectList: ProjectProps[] = [
     description:
       "A weekly planning tool built with Svelte, TypeScript, and Esbuild, integrated into Obsidian. Features dynamic templates powered by Svelte 5 reactivity, Markdown-enabled cells via Milkdown, and custom data structures for managing tasks and daily actions.",
     tags: ["Svelte", "TypeScript", "Esbuild", "Obsidian", "Milkdown", "State Management"],
-    link: "#",
+    link: "https://github.com/caasion/obsidian-ultimate-planner",
+    img: ultimatePlanner,
   },
   {
     title: "AI To-Do List",
     description:
       "An AI-assisted to-do list built with React, TypeScript, and Vite. Uses Firebase to embed a ChatGPT-like assistant that generates tasks and suggests plans based on energy levels, with core task management features and experiments in prompt engineering.",
     tags: ["React", "TypeScript", "Vite", "Firebase", "AI", "Prompt Engineering"],
-    link: "#",
+    link: "https://github.com/caasion/ai-todo-list",
+    img: aiTodoList,
   },
   {
     title: "Typing Transformer",
     description:
       "Contributed to an open-source Obsidian plugin with 9,000+ users. Improved plugin stability and performance by refactoring TypeScript and Svelte 5 code, authored clear documentation, and collaborated with maintainers through GitHub issues and PRs.",
     tags: ["Open Source", "TypeScript", "Svelte 5", "GitHub Collaboration", "Documentation"],
-    link: "#",
+    link: "https://github.com/aptend/typing-transformer-obsidian",
+    img: typingTransformer,
   },
   {
     title: "React Movie App",
     description:
       "A React.js app that integrates with The Movie Database (TMDB) API to fetch and display films. Features reusable components like a movie card and search bar, styled with Tailwind, and exploration of Appwrite for trending search tracking.",
     tags: ["React", "TMDB API", "TailwindCSS", "Appwrite", "Frontend Development"],
-    link: "#",
+    link: "https://github.com/caasion/react-movie-app",
+    img: reactMovieApp,
   },
   {
-    title: "Learner’s Lodge",
+    title: "Learner's Lodge",
     description:
       "A high school club I founded to explore effective learning strategies and build a community of learners. Organized biweekly workshops on memory, focus, and study techniques inspired by neuroscience, reaching 30+ students with positive feedback.",
     tags: ["Leadership", "Workshops", "Community Building", "Education", "Neuroscience"],
-    link: "#",
+    link: "/llx",
+    img: llx,
   },
 ];
 
@@ -43,10 +53,13 @@ const projectList: ProjectProps[] = [
 const Projects = () => {
   return (
     <div className='mt-16 mx-60'>
-      <h1 className='text-6xl text-bold pt-4'>Projects</h1>
-      {projectList.map(({title, description, tags, link}) => (
-        <ProjectCard title={title} description={description} tags={tags} link={link} />
-      ))}
+      <h1 className='text-6xl text-bold py-4'>Projects</h1>
+      <div className="flex-col row-gap-4">
+        {projectList.map(({title, description, tags, link, img}) => (
+          <ProjectCard title={title} description={description} tags={tags} link={link} img={img}/>
+        ))}
+      </div>
+      
     </div>
   )
 }
